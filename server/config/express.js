8af549faa4371af9ef22fb047f8f9065d5be6963
961018 <cho@DESKTOP-SJSVGO6>
2024-04-22 13:30:26 +0900

    to push aws

--- server/config/express.js
+++ server/config/express.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 // const auth = require('../middlewares/auth');
 const cors = require('cors');
+const whitelist=['https://eun.manoit.kr/','https://d5sii0aelmrln.cloudfront.net/','http://eun.manoit.kr.s3-website-ap-southeast-1.amazonaws.com'];
 module.exports = (app) => {
-    app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+    app.use(cors({origin: whitelist, credentials: true}));
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ limit: '50mb', extended: true }));
     app.use(cookieParser());
